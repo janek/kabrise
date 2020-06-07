c18ffe10895a32b627c97056bb3fcba0f00e6a24
Janek Szynal <jan.szynal@gmail.com>
2020-06-07 20:52:16 +0200

    Test change

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,18 +1,17 @@
-import React from "react"
-import { Styled } from "theme-ui"
+import React from 'react';
+import { Styled } from 'theme-ui';
 
 /**
  * Change the content to add your own bio
  */
 
 export default function Bio() {
-  return (
-    <>
-      This is where <Styled.a href="http://example.com/">your name</Styled.a>
-      {` `}
-      goes.
-      <br />
-      Or whatever, you make the rules.
-    </>
-  )
+	return (
+		<>
+			This is where <Styled.a href="http://example.com/">your name</Styled.a>
+			{` `}
+			goes.
+			<br />
+		</>
+	);
 }
