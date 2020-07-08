c557e4d75f733e32061ea3339a37550fd3ed3823
Janek Szynal <jan.szynal@gmail.com>
2020-07-08 16:25:29 +0200

    Add contact form

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -33,6 +33,21 @@ const Footer = () => {
           </Art>
           <FooterWrapper>
             <StyledContainer>
+              <form name="contact" method="POST" data-netlify="true">
+                <p>
+                  <label>
+                    Your Name: <input type="text" name="name" />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Your Email: <input type="email" name="email" />
+                  </label>
+                </p>
+                <p>
+                  <button type="submit">Send</button>
+                </p>
+              </form>
               <Copyright>
                 <span>
                   Instagram:{" "}
