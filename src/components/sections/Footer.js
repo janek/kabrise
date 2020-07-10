5ac1013360380fdea4035e463ceac5a777528325
Janek Szynal <jan.szynal@gmail.com>
2020-07-10 21:36:13 +0200

    Impressum & footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,15 +1,15 @@
-import React from "react"
-import styled from "styled-components"
-import { StaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react';
+import styled from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
-import { Container } from "@components/global"
-import ExternalLink from "@common/ExternalLink"
+import { Container } from '@components/global';
+import ExternalLink from '@common/ExternalLink';
 
 const Footer = () => {
-  return (
-    <StaticQuery
-      query={graphql`
+	return (
+		<StaticQuery
+			query={graphql`
         query {
           dancer_two: file(
             sourceInstanceName: { eq: "art" }
@@ -23,54 +23,51 @@ const Footer = () => {
           }
         }
       `}
-      render={data => (
-        <React.Fragment>
-          <Art>
-            <Img
-              fluid={data.dancer_two.childImageSharp.fluid}
-              style={{ width: 480, maxWidth: "100%", marginBottom: -16 }}
-            />
-          </Art>
-          <FooterWrapper>
-            <StyledContainer>
-              <form
-                name="contact"
-                method="POST"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="contact" value="contact" />
+			render={data => (
+				<React.Fragment>
+					<Art>
+						<Img fluid={data.dancer_two.childImageSharp.fluid} style={{ width: 480, maxWidth: '100%', marginBottom: -16 }} />
+					</Art>
+					<FooterWrapper>
+						<StyledContainer>
+							<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+								{/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+								<input type="hidden" name="contact" value="contact" />
 
-                <p>
-                  <label>
-                    Your Name: <input type="text" name="name" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Email: <input type="email" name="email" />
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
-              <Copyright>
-                <span>
-                  Instagram:{" "}
-                  <ExternalLink href="https://instagram.com/kabrise_">
-                    <b>@kabrise_</b>
-                  </ExternalLink>
-                </span>
-              </Copyright>
-            </StyledContainer>
-          </FooterWrapper>
-        </React.Fragment>
-      )}
-    />
-  )
-}
+								<p>
+									<label>
+										Your Name: <input type="text" name="name" />
+									</label>
+								</p>
+								<p>
+									<label>
+										Your Email: <input type="email" name="email" />
+									</label>
+								</p>
+								<p>
+									<button type="submit">Send</button>
+								</p>
+							</form>
+							<Copyright>
+								<span>
+									<ExternalLink href="/impressum/">
+										<b>Impressum</b>
+									</ExternalLink>
+								</span>
+								<span>
+									Instagram:{' '}
+									<ExternalLink href="https://instagram.com/kabrise_">
+										<b>@kabrise_</b>
+									</ExternalLink>
+								</span>
+							</Copyright>
+						</StyledContainer>
+					</FooterWrapper>
+				</React.Fragment>
+			)}
+		/>
+	);
+};
 const SocialIcons = styled.div`
   display: flex;
 
@@ -83,12 +80,12 @@ const SocialIcons = styled.div`
   @media (max-width: ${props => props.theme.screen.sm}) {
     margin-top: 40px;
   }
-`
+`;
 
 const FooterWrapper = styled.footer`
   background-color: ${props => props.theme.color.primary};
   padding: 32px 0;
-`
+`;
 
 const Copyright = styled.div`
   font-family: ${props => props.theme.font.secondary};
@@ -99,14 +96,14 @@ const Copyright = styled.div`
     text-decoration: none;
     color: inherit;
   }
-`
+`;
 
 const Art = styled.figure`
   display: flex;
   justify-content: center;
   margin: 0;
   margin-top: 48px;
-`
+`;
 
 const StyledContainer = styled(Container)`
   display: flex;
@@ -117,6 +114,6 @@ const StyledContainer = styled(Container)`
     flex-direction: column;
     text-align: center;
   }
-`
+`;
 
-export default Footer
+export default Footer;
---
+++ src/pages/impressum.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Layout from '@common/Layout';
+import Footer from '@sections/Footer';
+import { Section, Container } from '@components/global';
+
+const ImpressumPage = () => (
+	<Layout>
+		<Section>
+			<Container narrow>
+				<BackArrow>↜</BackArrow>
+				<h1> Impressum </h1>
+
+				<h4>Angaben gemäß §5 TMG</h4>
+
+				<p>
+					<i>Anschrift:</i>
+					<br />
+					Kabrise
+					<br />
+					Sonnenallee 102 <br />
+					12045 Berlin <br />
+					<br />
+					<i>Vertreten durch die geschäftsführende Gesellschafter: </i>
+					<br />
+					Janek Szynal, <br />
+					Fabian Benz <br />
+					<br />
+					<i>Kontakt:</i> <br />
+					Telefon: +49 (0) 177 1464495 <br />
+					E-Mail: kabrisekollektiv@gmail.com
+					<br /> <br />
+					<i>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</i>
+				</p>
+
+				<h4>Haftung für Inhalte</h4>
+				<p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+				<h4>Haftung für Links</h4>
+				<p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+				<h4>Urheberrecht</h4>
+				<p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+				<h4>Wir sind nicht bereit oder verpflichtet</h4>
+
+				<p>an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen. Haftung für Inhalte Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.</p>
+			</Container>
+		</Section>
+		<Footer />
+	</Layout>
+);
+
+const BackArrow = styled.span`
+	color: #3d5ea9;
+	font-size: 10em;
+	:hover {
+		color: black;
+	}
+`;
+
+export default ImpressumPage;
