1ab8d83e10bfe8d6885f488dd30795f12880b646
Janek Szynal <jan.szynal@gmail.com>
2020-09-26 20:14:10 +0200

    Revert "Remove the header (reference to open call)"
    This reverts commit 72c49669943bb40920f3709c60338b973fd3cff2.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React from "react"
 
-import Layout from '@common/Layout';
-import Navbar from '@common/Navbar';
+import Layout from "@common/Layout"
+import Navbar from "@common/Navbar"
 
-import Header from '@sections/Header';
-import About from '@sections/About';
-import Project from '@sections/Project';
-import Focus from '@sections/Focus';
+import Header from "@sections/Header"
+import About from "@sections/About"
+import Project from "@sections/Project"
+import Focus from "@sections/Focus"
 
-import Footer from '@sections/Footer';
+import Footer from "@sections/Footer"
 
 const IndexPage = () => (
 	<Layout>
 		<Navbar />
+		<Header />
 		<About />
 		<Project />
 		<Focus />
 		<Footer />
 	</Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
