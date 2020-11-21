71eade005fdb79efe70df0156bdf27a3c5cb6d78
Janek Szynal <jan.szynal@gmail.com>
2020-11-21 16:33:58 +0100

    Basic cleanup in Sidebar.js

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -8,94 +8,72 @@ import { Container } from "@components/global"
 import Logo from "@common/Logo"
 import ExternalLink from "@common/ExternalLink"
 
-
 // import Language from "./Language"
 
 import { ReactComponent as MenuIcon } from "@static/icons/menu.svg"
 
-
-export const StyledContainer = styled(Container)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-
 export const Mobile = styled.div`
-display: none;
-
-@media (max-width: ${props => props.theme.screen.md}) {
-  display: block;
-}
-
-${props =>
-  props.hide &&
-  `
-  display: block;
-
-  @media (max-width: ${props.theme.screen.md}) {
-    display: none;
+  display: none;
+  
+  @media (max-width: ${props => props.theme.screen.md}) {
+    display: block;
   }
-`}
-`
-
-export const NavListWrapper = styled.div`
-display:  flex;
-flex-direction: column;
-justify-content: space-between;
-font-family: ${props => props.theme.font.menu};
-${props => props.theme.font_size.regular};
-
-ul {
-  height: 40vh;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  ${({ mobile }) =>
-    mobile &&
+  
+  ${props =>
+    props.hide &&
     `
-      flex-direction: column;
-      margin-top: 1em;
-
-      > ${NavItem} {
-        margin: 0;
-        margin-top: 0.75em;
-      }
-    `};
-}
-
-li {
-  transform: rotate(-90deg);
-  margin: 0 0.75em;
-
-  a {
-    text-decoration: none;
-    opacity: 0.7;
-    color: ${props => props.theme.color.primary};
-
-    :hover {
-      text-decoration: underline;
+    display: block;
+  
+    @media (max-width: ${props.theme.screen.md}) {
+      display: none;
     }
-  }
-
-  &.active {
-    a {
-      opacity: 1;
-    }
-  }
-}
+  `}
 `
-
-export const Nav = styled.nav`
+// TODO: reconsider naming between here and Navbar
+export const NavListWrapper = styled.div`
   padding: 16px 0;
   position: fixed;
   width: 10vw;
   height: 100vh;
   top: 0;
   z-index: 1000;
+  display:  flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-family: ${props => props.theme.font.menu};
+  ${props => props.theme.font_size.regular};
+  
+  ul {
+    height: 40vh;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  
+  }
+  
+  li {
+    transform: rotate(-90deg);
+    margin: 0 0.75em;
+  
+    a {
+      text-decoration: none;
+      opacity: 0.7;
+      color: ${props => props.theme.color.primary};
+  
+      :hover {
+        text-decoration: underline;
+      }
+    }
+  
+    &.active {
+      a {
+        opacity: 1;
+      }
+    }
+  }
 `
 
 export const NavItem = styled.li`
@@ -133,12 +111,11 @@ const Sidebar = props => {
     return intl.formatMessage({ id: targetString })
   })
 
-  const getNavList = ({ mobile = false }) => (
-    <NavListWrapper mobile={mobile}>
+  const getNavList = () => (
+    <NavListWrapper>
       <Scrollspy
         items={NAV_ITEMS_RAW.map(item => item.toLowerCase())}
         currentClassName="active"
-        mobile={mobile}
         offset={-64}
       >
         {NAV_ITEMS_RAW.map(navItem => (
@@ -152,11 +129,10 @@ const Sidebar = props => {
   )
 
   return (
-    <Nav {...props}>
-      <StyledContainer>
-        <Mobile hide>{getNavList({})}</Mobile>
-      </StyledContainer>
-    </Nav>
+    //XXX: should we pass down props? (used to be <Nav {...props}>). Should be no problem, but is it desired?
+      <Mobile hide>
+        {getNavList({})}
+      </Mobile>
   )
 }
 
