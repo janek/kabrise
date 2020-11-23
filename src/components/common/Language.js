c093870371b9cf173b58b2f378aac4289125230e
Janek Szynal <jan.szynal@gmail.com>
2020-11-23 19:29:02 +0100

    Fix typo and lint language.js

--- src/components/common/Language.js
+++ src/components/common/Language.js
@@ -4,12 +4,12 @@ import { IntlContextConsumer, changeLocale } from "gatsby-plugin-intl"
 
 const languageName = {
   en: "en",
-  de: "de"
+  de: "de",
 }
 
 const LineSeparator = styled.div`
   background-color: white;
-  margin: 0 auto
+  margin: 0 auto;
   width: 1px;
   height: 40px;
 `
@@ -17,37 +17,37 @@ const LineSeparator = styled.div`
 const Language = () => {
   return (
     <div>
-        <IntlContextConsumer>
-          {({ currentLocale }) =>
-            <div>
-              <li class="language" key={"en"}>
-                <a
-                  key={"en"}
-                  onClick={() => changeLocale("en")}
-                  style={{
-                    cursor: `pointer`,
-                    opacity: currentLocale === "en" ? 1 : 0.7
-                  }}
-                >
-                  {"en".toUpperCase()}
-                </a>
-              </li>
-              <LineSeparator/>
-              <li class="language" key={"de"}>
-                <a
-                  key={"de"}
-                  onClick={() => changeLocale("de")}
-                  style={{
-                    cursor: `pointer`,
-                    opacity: currentLocale === "de" ? 1 : 0.7
-                  }}
-                >
-                  {"de".toUpperCase()}
-                </a>
-              </li>
-            </div>
-          }
-        </IntlContextConsumer>
+      <IntlContextConsumer>
+        {({ currentLocale }) => (
+          <div>
+            <li class="language" key={"en"}>
+              <a
+                key={"en"}
+                onClick={() => changeLocale("en")}
+                style={{
+                  cursor: `pointer`,
+                  opacity: currentLocale === "en" ? 1 : 0.7,
+                }}
+              >
+                {"en".toUpperCase()}
+              </a>
+            </li>
+            <LineSeparator />
+            <li class="language" key={"de"}>
+              <a
+                key={"de"}
+                onClick={() => changeLocale("de")}
+                style={{
+                  cursor: `pointer`,
+                  opacity: currentLocale === "de" ? 1 : 0.7,
+                }}
+              >
+                {"de".toUpperCase()}
+              </a>
+            </li>
+          </div>
+        )}
+      </IntlContextConsumer>
     </div>
   )
 }
