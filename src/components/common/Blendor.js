69b1464947424c4c0b64c0d4554e82ca9c6a0007
Janek Szynal <jan.szynal@gmail.com>
2020-12-28 13:48:52 +0100

    Simplify blendor

--- src/components/common/Blendor.js
+++ src/components/common/Blendor.js
@@ -2,21 +2,15 @@ import styled from "styled-components"
 import React from "react"
 
 // XXX: https://stackoverflow.com/questions/33223603/using-css-mix-blend-mode-with-a-child-of-a-position-fixed-element
-const BlendorBackground = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: blue;
-  mix-blend-mode: difference;
+const BlendText = styled.h1`
+  mix-blend-mode: exclusion;
+  color: white;
   display: block;
   position: fixed;
   top: 0px;
   left: 0px;
 `
 
-const Blendor = () => (
-  <BlendorBackground>
-    <span> HELLO </span>
-  </BlendorBackground>
-)
+const Blendor = () => <BlendText> HELLO </BlendText>
 
 export default Blendor
