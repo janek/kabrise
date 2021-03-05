fd0e4d7502a0eab519148e8a83b2860b6c6979c1
Janek Szynal <jan.szynal@gmail.com>
2021-03-05 14:38:20 +0100

    Add "scroll down" arrows

--- src/components/sections/Video.js
+++ src/components/sections/Video.js
@@ -27,6 +27,14 @@ const Video = () => {
             <VideoPlayer loop muted playsinline autoPlay>
               <source src={BriseVideo} type="video/mp4" />
             </VideoPlayer>
+            <ScrollDownWrapper>
+              <ScrollDownButton>Scroll down</ScrollDownButton>
+              <Arrow>
+                <span></span>
+                <span></span>
+                <span></span>
+              </Arrow>
+            </ScrollDownWrapper>
           </VideoWrapper>
         </section>
       )}
@@ -38,6 +46,7 @@ const VideoWrapper = styled.div`
   min-height: 500px;
   background-color: grey;
   height: 100vh;
+  text-align: center;
 `
 
 // XXX: We most likely need media queries for mobile screens
@@ -45,9 +54,63 @@ const VideoWrapper = styled.div`
 const VideoPlayer = styled.video`
   object-fit: cover;
   width: 100vw;
-
   top: 0;
   left: 0;
+`
+
+const ScrollDownWrapper = styled.div`
+  position: absolute;
+  bottom: -20px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 200px;
+  height: 100px;
+`
+
+const ScrollDownButton = styled.button`
+  font-size: 15px;
+  text-transform: uppercase;
+  color: white;
+  font-weight: bold;
+`
+const Arrow = styled.div`
+  position: absolute;
+  bottom: 70px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  span {
+    display: block;
+    width: 10px;
+    height: 10px;
+    border-bottom: 2px solid white;
+    border-right: 2px solid white;
+    transform: rotate(45deg);
+    margin: -5px;
+    animation: animate 2s infinite;
+  }
+  span:nth-child(2) {
+    animation-delay: -0.2s;
+  }
+  span:nth-child(3) {
+    animation-delay: -0.4s;
+  }
+  @keyframes animate {
+    0% {
+      opacity: 0;
+      transform: rotate(45deg) translate(-10px, -10px);
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+      transform: rotate(45deg) translate(0px, 0px);
+    }
+  }
 `
 
 export default Video
